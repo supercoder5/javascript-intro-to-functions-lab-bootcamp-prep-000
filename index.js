354fd3a1a2f9282ccf0ac57b7accabe3530636c0
Daniel Smith <the.dan.s.smith@gmail.com>
2017-09-26 00:35:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ var uppercase, lowercase
 
 function shout(string){
   uppercase = string.toUpperCase();
+  string = uppercase;
   return uppercase;
 }
 function whisper(string){
   lowercase = string.toLowerCase();
+  string = lowercase;
   return lowercase;
 }
 function logShout(string){
   uppercase = string.toUpperCase()
+  string = uppercase;
   console.log(uppercase);
 }
 function logWhisper(string){
@@ -17,14 +20,4 @@ function logWhisper(string){
   string = uppercase;
   console.log(lowercase);
 }
-function sayHiToGrandma(string){
-    if (string.toLowerCase() === string){
-      return 'I can\'t hear you!';
-    }
-    if (string.toUpperCase() === string){
-      return 'YES INDEED!';
-    }
-    if (string === 'I love you, Grandma.'){
-      return 'I love you, too.';
-    }
-}
+
