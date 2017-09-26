7b077278992ec3dc370c324aaadf1ee1cc8f3218
Daniel Smith <the.dan.s.smith@gmail.com>
2017-09-26 00:34:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ var uppercase, lowercase
 
 function shout(string){
   uppercase = string.toUpperCase();
-  string = uppercase;
   return uppercase;
 }
 function whisper(string){
   lowercase = string.toLowerCase();
-  string = lowercase;
   return lowercase;
 }
 function logShout(string){
   uppercase = string.toUpperCase()
-  string = uppercase;
   console.log(uppercase);
 }
 function logWhisper(string){
